6d205cd43a8a71c2ee77b46694a70a6745b6131d
1Chigo <sixteenstomatos@gmail.com>
2025-01-01 16:30:30 +0800

    lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,12 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSquarePrimesMulti() {
+        IntList lst = IntList.of(14, 15, 16, 17, 19);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("14 -> 15 -> 16 -> 289 -> 361", lst.toString());
+        assertTrue(changed);
+    }
 }
