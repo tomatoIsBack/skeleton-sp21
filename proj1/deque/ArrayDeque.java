69f086ca8a3ef4e60d1e92331e365b948c5f6898
1Chigo <sixteenstomatos@gmail.com>
2025-01-02 21:23:07 +0800

    arraydeque

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,138 @@
+package deque;
+
+import java.util.Iterator;
+
+public class ArrayDeque<T> implements Deque<T>, Iterable<T>{
+    private T[] items;
+    private int size;
+    private int start;
+
+    public ArrayDeque() {
+        items = (T[]) new Object[8];
+        size = 0;
+    }
+
+    private void expandArrays() {
+        if (size == items.length) {
+            T[] newitems = (T[]) new Object[items.length * 4];
+            int newStart = Math.floorMod(start, items.length);
+            if (newStart == 0) {
+                System.arraycopy(items, 0, newitems, 0, items.length);
+            } else {
+                System.arraycopy(items, newStart, newitems, 0, items.length - newStart);
+                System.arraycopy(items, 0, newitems, items.length - newStart, newStart);
+            }
+            start = 0;
+            items = newitems;
+        }
+    }
+
+    @Override
+    public void addFirst(T item) {
+        expandArrays();
+        start = Math.floorMod(start - 1, items.length);
+        items[start] = item;
+        size++;
+    }
+
+    @Override
+    public void addLast(T item) {
+        expandArrays();
+        items[Math.floorMod(start + size, items.length)] = item;
+        size++;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void printDeque() {
+        for (T i : this) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    private class DequeIterator implements Iterator<T> {
+        private int count;
+        public DequeIterator() {
+            count = size;
+        }
+        @Override
+        public boolean hasNext() {
+            return count > 0;
+        }
+
+        @Override
+        public T next() {
+            count--;
+            return items[Math.floorMod(start + size - count - 1, items.length)];
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ArrayDeque.DequeIterator();
+    }
+
+    @Override
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T item = items[start];
+        start = Math.floorMod(start + 1, items.length);
+        size--;
+        return item;
+    }
+
+    @Override
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T item = items[Math.floorMod(start + size - 1, items.length)];
+        size--;
+        return item;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size) {
+            return null;
+        }
+        return items[Math.floorMod(start + index, items.length)];
+    }
+
+    public boolean equals(Object o) {
+        /*
+        if (o instanceof Deque other) {
+            if (this.size() == other.size()) {
+                for (int i = 0; i < this.size(); i++) {
+                    if (this.get(i) != other.get(i)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+         */
+        Deque other = (ArrayDeque) o;
+        if (this.size() == other.size()) {
+            for (int i = 0; i < this.size(); i++) {
+                if (this.get(i) != other.get(i)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
